fd9fe87db51bea164c36205c0508d0afc60e3427
Daniel Cordeiro Junior <daniel_c_jr@yahoo.com.br>
2023-11-25 12:03:57 -0300

    Ajustando layout - pedido so aparece depois de selecionado algum produto

--- app.py
+++ app.py
@@ -22,22 +22,35 @@ colunapedido=['cod_produto','desc_produto','pedido']
 dfpedido = pd.DataFrame(columns=colunapedido)
 
 # Mostra a tabela mãe para apoio da seleção de quantidades
-#st.dataframe(dftab[colunas], hide_index=True)
+st.dataframe(dftab[colunas], hide_index=True)
 
 # Funcão para retornar as quantidades de cada produto no selectbox
-def get_quantidades(produto):
+def get_quantidades(codigo):
     # Selecionar as colunas de quantidade e converter para uma lista
-    quantidades = dftab[['quantidade1', 'quantidade2', 'quantidade3', 'quantidade4']].values.tolist()[0]
-    return quantidades
+    if codigo:
+        quantidades = None
+        quantidades = dftab.loc[dftab['cod_produto'] == codigo, ['quantidade1', 'quantidade2', 'quantidade3', 'quantidade4']].values[0]
+        #quantidades = dftab[['quantidade1', 'quantidade2', 'quantidade3', 'quantidade4']].values.tolist()[0]
+        return quantidades
 
 # Funcão para atualizar dados no Dataframe
-def atualiza_dftab(produto,quantidade):
-    # Extrai o codigo do produto
-    codproduto = dftab.loc[dftab['desc_produto'] == selecao_prod, 'cod_produto'].values[0]
+def atualiza_dftab(codproduto,produto,quantidade):
     #mask = (dftab['cod_produto'].values)
     #dftab.loc[dftab['cod_produto'] == codproduto, 'pedido'] = quantidade
     selecao=[codproduto,produto,quantidade]
     dfpedido.loc[len(dfpedido)] = selecao
+
+# Cria botoes de função
+def cria_botao():
+    col1, col2 = st.columns(2)
+    with col1:
+    # Cria botão para limpar Pedido(em construção)
+        if st.button("Enviar Pedido"):
+            dfpedido = ""
+    with col2:
+    # Cria botão enviar Pedido(em construção)
+        if st.button("Limpar Pedido"):
+            dfpedido = ""
 
 st.markdown(f"\n")
 st.markdown(f"<h3 style='text-align: center; color: black;'>Guia de Pedidos</h3>", unsafe_allow_html=True)
@@ -45,31 +58,30 @@ st.markdown(f"<h3 style='text-align: center; color: black;'>Guia de Pedidos</h3>
 selecao_prod=st.selectbox(f'****Produtos Disponíveis:****',dftab['desc_produto'],
                                                          placeholder=msg_produto,
                                                          index=None)
+# Extrai o codigo do produto
+if selecao_prod:
+    codproduto = dftab.loc[dftab['desc_produto'] == selecao_prod, 'cod_produto'].values[0]
+
 # Selecionar as quantidades desejadas
-selecao_qtd=st.selectbox(f'****Quantidades Disponíveis:****', [*get_quantidades(selecao_prod)],
+    selecao_qtd=st.selectbox(f'****Quantidades Disponíveis:****', [*get_quantidades(codproduto)],
                                                             placeholder="Selecione a quantidade...",
                                                             index=None)
-
 # Chama função que insere os valores selecionados no datafreme
-if selecao_prod is not None:
-    atualiza_dftab(selecao_prod,selecao_qtd)
-
-
-
-
+    if selecao_prod is not None:
+        atualiza_dftab(codproduto,selecao_prod,selecao_qtd)
 
 # Somantório de quantidades
-soma = pd.to_numeric(dfpedido['pedido'], errors='coerce').sum()
+    soma = pd.to_numeric(dfpedido['pedido'], errors='coerce').sum()
 
-st.markdown(f"\n")
-st.markdown(f"\t<h3 style='text-align: center; color: black;'>Pedido</h3>", unsafe_allow_html=True)
+    st.markdown("\n")
+    st.markdown("\n")
+    st.markdown(f"\t<h3 style='text-align: center; color: black;'>Pedido</h3>", unsafe_allow_html=True)
+    st.markdown("\n")
+    st.markdown("\n")
 
-# Cria botão enviar Pedido(em construção)
-#if st.button("Enviar Pedido"):
-#    df_final = None
-df_pedido = dftab[dftab['pedido'] != "nan"]
-st.dataframe(dfpedido, hide_index=True)
-st.write(f'Seu pedido tem um total de: {int(soma.round(0))} itens')
-# Cria botão para limpar Pedido(em construção)
-#if st.button("Limpar"):
-#    df_final = None
+    if dfpedido.notnull:
+        cria_botao()
+        st.dataframe(dfpedido, hide_index=True)
+        st.write(f'Seu pedido tem um total de: {int(soma.round(0))} itens')
+    else:
+        st.markdown(f"\n")
