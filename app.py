9c6a8ccbd4f5ce7d5c46a46db865e88e4cf1ba13
Daniel Cordeiro Junior <daniel_c_jr@yahoo.com.br>
2023-12-04 14:02:58 -0300

    Alterado a forma de entrada de dados para quantidade

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ st.image('logo.jpeg')
 st.markdown(f"\n")
 
 # Carrega a tabela com Mercadorias e Quantidades a serem trabalhadas
-dftab = pd.read_excel(f'mercadoria.xlsx')
-original_df=dftab.copy()
+dftab = pd.read_excel(f'C:\Projetos_Phayton\Primeiro_Dashboard\mercadoria.xlsx')
+
+dftab.index = pd.RangeIndex(start=1, stop=len(dftab) + 1)
 
 # Formata os campos da tabela de dados
 dftab = dftab.astype({'cod_produto': 'str', 'desc_produto': 'str',
                       'minimo': 'str', 'qtd2': 'str',
-                      'qtd3': 'str', 'qtd4': 'str', 'pedido': 'str'
+                      'qtd3': 'str', 'qtd4': 'str'
 })
+
+st.markdown(f"\n")
+
 def get_quantidades(codigo):
     # Selecionar as colunas de quantidade e converter para uma lista
     if codigo:
@@ -21,56 +25,62 @@ def get_quantidades(codigo):
         quantidades = dftab.loc[dftab['cod_produto'] == codigo, ['minimo','qtd2','qtd3','qtd4']].values[0]
         return quantidades
 
+def exporta_pedido(dfpedido):
+    dfpedido.to_csv("pedido_loja.csv", index=False, header=False)
+
 if 'num' not in st.session_state:
-    st.session_state.num = 1
+    st.session_state.num = 0
 if 'data' not in st.session_state:
     st.session_state.data = []
 
 class NovoPedido:
     def __init__(self, page_id):
-        # Selecionar o produto desejado
-        selecao_prod=st.selectbox(f'****Produtos Disponíveis:****',dftab['desc_produto'],
-                                                         placeholder="Selecione o produto...")
-        # Extrai o codigo do produto
-        #if selecao_prod:
-        codproduto = dftab.loc[dftab['desc_produto'] == selecao_prod, 'cod_produto'].values[0]
+        idx_produto = page_id
+        idx_quantidade = 0
+        self.produto = self.selecionaproduto(idx_produto)
+        self.codigo = self.retorna_codigo(self.produto)
 
-        # Selecionar as quantidades desejadas
-        selecao_qtd=st.selectbox(f'****Quantidades Disponíveis:****', [*get_quantidades(codproduto)],
-                                                            placeholder="Selecione a quantidade...")
-        self.codproduto=codproduto
-        self.produto = selecao_prod
-        self.quantidade = selecao_qtd
+    def selecionaproduto(self, idx_produto):
+        produto = st.selectbox(f'****Produtos Disponíveis:****', dftab['desc_produto'],
+                               index=idx_produto,
+                               placeholder="Selecione o produto...")
+        return produto
+
+    def retorna_codigo(self, selecao_prod):
+        codigo = dftab.loc[dftab['desc_produto'] == selecao_prod, 'cod_produto'].values[0]
+        return codigo
 
 def main():
     placeholder = st.empty()
     placeholder2 = st.empty()
-
-    while True:
+    while True: 
         num = st.session_state.num
         if placeholder2.button('Enviar Pedido', key=num):
             placeholder2.empty()
             dfpedido = pd.DataFrame(st.session_state.data)
             dfpedido = dfpedido.drop_duplicates(subset='Produto').reset_index(drop=True)
             st.dataframe(dfpedido, hide_index=True)
+            exporta_pedido(dfpedido) 
+            st.session_state.num = 1
+            st.session_state.data = []         
             break
         else:
            with placeholder.form(key=str(num)):
-               new_produto = NovoPedido(page_id=num)
+               novo_produto = NovoPedido(page_id=num)
+               quantidade=st.text_input(label='****Insira a Quantidade Desejada****')
                if st.form_submit_button('Adicionar'):
                    st.session_state.data.append({
-                       'Codigo': new_produto.codproduto,
-                       'Produto': new_produto.produto,
-                       'Quantidade': new_produto.quantidade
+                       'Codigo': novo_produto.codigo,
+                       'Produto': novo_produto.produto,
+                       #'Quantidade': novo_produto.quantidade
+                       'Quantidade': quantidade
                    })
                    dfpedido = pd.DataFrame(st.session_state.data)
                    dfpedido = dfpedido.drop_duplicates(subset='Produto').reset_index(drop=True)
                    st.dataframe(dfpedido, hide_index=True)
                    st.session_state.num += 1
-                   placeholder.empty()
-                   placeholder2.empty()
-                elif st.form_submit_button('Adicionar'):
-                   dfpedido = dfpedido.query('Codigo == new_produto.codproduto and Produto == "new_produto.produto"')
                else:
-                   st.stop()
+                   st.stop()        
 main()
+if st.button('Novo Pedido'):
+    st.experimental_rerun()
