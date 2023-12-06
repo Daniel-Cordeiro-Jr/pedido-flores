e401307c11fe61812ff43ac6480ee3838d112bd2
Daniel Cordeiro Junior <daniel_c_jr@yahoo.com.br>
2023-12-06 16:58:45 -0300

    Alterado estilos da pagina

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+
+# Carregar o arquivo style.css
+with open ("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 # Carrega a imagem do logo
 st.image('logo.jpeg')
-st.markdown(f"\n")
 
 # Carrega a tabela com Mercadorias e Quantidades a serem trabalhadas
 dftab = pd.read_excel(f'mercadoria.xlsx')
@@ -15,8 +19,6 @@ dftab = dftab.astype({'cod_produto': 'str', 'desc_produto': 'str',
                       'minimo': 'str', 'qtd2': 'str',
                       'qtd3': 'str', 'qtd4': 'str'
 })
-
-st.markdown(f"\n")
 
 def get_quantidades(codigo):
     # Selecionar as colunas de quantidade e converter para uma lista
@@ -52,34 +54,41 @@ class NovoPedido:
 def main():
     placeholder = st.empty()
     placeholder2 = st.empty()
+
     while True: 
         num = st.session_state.num
-        if placeholder2.button('Enviar Pedido', key=num):
+        if placeholder2.button(f'***Enviar Pedido***', key=num):
             placeholder2.empty()
             dfpedido = pd.DataFrame(st.session_state.data)
             dfpedido = dfpedido.drop_duplicates(subset='Produto').reset_index(drop=True)
             st.dataframe(dfpedido, hide_index=True)
-            exporta_pedido(dfpedido) 
+            #exporta_pedido(dfpedido) 
             st.session_state.num = 1
             st.session_state.data = []         
             break
         else:
            with placeholder.form(key=str(num)):
                novo_produto = NovoPedido(page_id=num)
-               quantidade=st.text_input(label='****Insira a Quantidade Desejada****')
-               if st.form_submit_button('Adicionar'):
-                   st.session_state.data.append({
-                       'Codigo': novo_produto.codigo,
-                       'Produto': novo_produto.produto,
-                       #'Quantidade': novo_produto.quantidade
-                       'Quantidade': quantidade
-                   })
-                   dfpedido = pd.DataFrame(st.session_state.data)
-                   dfpedido = dfpedido.drop_duplicates(subset='Produto').reset_index(drop=True)
-                   st.dataframe(dfpedido, hide_index=True)
-                   st.session_state.num += 1
+               vl_quantidade=st.text_input(label=f'****Quantidade Desejada****')
+               if st.form_submit_button('***Adicionar***'):
+                    if vl_quantidade != "":
+                        st.session_state.data.append({
+                        'Codigo': novo_produto.codigo,
+                        'Produto': novo_produto.produto,
+                        'Quantidade': vl_quantidade
+                        })
+                        dfpedido = pd.DataFrame(st.session_state.data)
+                        dfpedido = dfpedido.drop_duplicates(subset='Produto').reset_index(drop=True)
+                        st.dataframe(dfpedido, hide_index=True)
+                        st.session_state.num += 1
+                    elif vl_quantidade == "":
+                        dfpedido = pd.DataFrame(st.session_state.data)
+                        dfpedido = dfpedido.drop_duplicates(subset='Produto').reset_index(drop=True)
+                        st.dataframe(dfpedido, hide_index=True)
+                        st.session_state.num += 1                        
                else:
                    st.stop()        
+                   
 main()
 if st.button('Novo Pedido'):
     st.experimental_rerun()
